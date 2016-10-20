f248e1da252b946521422b5019e540077190fd4b
Le Thi Cuc <le.thi.cuc@framgia.com>
2016-10-20 17:21:02 +0700

    Create order

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: "registrations",
     omniauth_callbacks: "users/omniauth_callbacks"}
   resources :products
+  resources :carts
 end
